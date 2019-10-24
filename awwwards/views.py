aa0757e10cc71be1ebffdc2fc384cf84c856441a
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-24 16:28:03 +0300

    Importing post fro model file, creating a template function and returning it to the home page

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def HomePageView(request):
+    # model = Post
+    post = Post.objects.all()
+    return render(request,'pages/home.html')
